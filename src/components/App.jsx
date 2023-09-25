0294b0934a5ed519a5c119d061e0dd966de5619d
Yura <yuraplatonov2000@gmail.com>
2023-09-25 16:49:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import Header from './Header/Header';
-import Main from './Main/Main';
-import Footer from './Footer/Footer';
+
 
 
 export const App = () => {
   return (
     <div className="app">
       <Header />
-      {/* <Main /> */}
-      {/* <Footer /> */}
+      
     </div>
   );
 };
