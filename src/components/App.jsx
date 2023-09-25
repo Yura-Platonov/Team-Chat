423a3a2c148d73def17b6950045e411817214875
anna <anutka2610@gmail.com>
2023-09-25 17:45:54 +0200

    added home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from './Header/Header';
-
-
+import Home from './Home/Home';
 
 export const App = () => {
   return (
     <div className="app">
       <Header />
-      
+      <Home />
     </div>
   );
 };
-
