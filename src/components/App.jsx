bacd394bcc62a4f83ddc89fd7ad4ca9cd037937b
Yura <yuraplatonov2000@gmail.com>
2023-09-27 17:46:04 +0300

    add main section and fix switch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header/Header';
-import Home from './Home/Home';
+import Main from './Main/Main';
+
 
 export const App = () => {
   return (
     <div className="app">
       <Header />
-      <Home />
+      <Main />
     </div>
   );
 };
