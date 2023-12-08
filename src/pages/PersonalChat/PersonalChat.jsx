b531bb35035864082ea8ec3f3b987c7bcd0ab6a1
Yura <yuraplatonov2000@gmail.com>
2023-12-08 16:15:34 +0200

    made model of page

--- src/pages/PersonalChat/PersonalChat.jsx
+++ src/pages/PersonalChat/PersonalChat.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-// import { Outlet } from 'react-router';
+import PersonalChatImg from '../../components/Images/PersonalChatImg.png';
 
 import css from './PersonalChat.module.css';
 
 function PersonalChat() {
-  return (
-    <div>
-      <section className={css.welcome}>
-        <h1 className={css.welcome_title}>Welcome every tourist <br/> to Teamchat</h1>
-        <p className={css.welcome_text}>Chat about a wide variety of tourist equipment.<br/>Communicate, get good advice and choose!</p>
-      </section>
-      
-      
-    </div>
-    
-  );
+    return (
+        <div className={css.container}>
+            <h2 className={css.title}>Your personal chats</h2>
+            <div className={css.main_container}>
+                <img className={css.noChats_img} src={PersonalChatImg} alt="Personal Chat Img" />
+                <p className={css.noChats_text}>Your personal chats will be here soon</p>
+            </div>
+        </div>
+      );
 }
 
 export default PersonalChat;
