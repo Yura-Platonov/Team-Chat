eb049d91c1f863e1ecdd961e9250839aac8289cc
Yura <yuraplatonov2000@gmail.com>
2024-01-29 17:38:45 +0200

    change logo

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoLight from '../Images/LogoLight.svg';
-import LogoDark from '../Images/LogoDark.svg';
+import NewLogoLight from '../Images/NewLogoLight.svg';
+import NewLogoDark from '../Images/NewLogoDark.svg';
 import css from'./Logo.module.css';
 
 const Logo = ({ darkTheme }) => {
     return (
       <Link to="/">
       <img
-        src={darkTheme ? LogoDark : LogoLight}
+        src={darkTheme ? NewLogoDark : NewLogoLight}
         alt="Logo company"
         className={css.logo}
       />
