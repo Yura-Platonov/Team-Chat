6923b2a0603f2546b34babb6d5fe75f73d39c890
Yura <yuraplatonov2000@gmail.com>
2024-02-09 18:29:43 +0200

    working at menu

--- src/components/Chat/UserMenu.jsx
+++ src/components/Chat/UserMenu.jsx
@@ -1,20 +1,15 @@
-// UserMenu.jsx
-
 import React from 'react';
 import css from './UserMenu.module.css';
 
-const UserMenu = ({ selectedUser, onClose, posX, posY }) => {
+const UserMenu = ({ selectedUser, onClose}) => {
   const handleDirectMessageClick = () => {
     console.log(`Direct message to ${selectedUser.user_name}`);
-    // Здесь можешь добавить логику отправки прямого сообщения
-    // Например, открыть модальное окно для написания сообщения
   };
 
   return (
-    <div className={css.userMenu} style={{ top: posY, left: posX }}>
+    <div className={css.userMenu} >
       <p>Write direct message to {selectedUser.user_name}</p>
       <button onClick={handleDirectMessageClick}>Write direct message</button>
-      {/* Добавим кнопку для закрытия меню */}
       <button onClick={onClose}>Close</button>
     </div>
   );
