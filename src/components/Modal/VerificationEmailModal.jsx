aa63c4d507049f68e7578e94bf24197c684a4004
Yura <yuraplatonov2000@gmail.com>
2024-02-23 00:57:19 +0200

    made modal welcome

--- src/components/Modal/VerificationEmailModal.jsx
+++ src/components/Modal/VerificationEmailModal.jsx
@@ -4,15 +4,25 @@ import css from './VerificationEmailModal.module.css';
 
 
 const VerificationEmailModal = ({ isOpen, onClose }) => {
+  // Получение имени пользователя и аватара из локального хранилища
+  const user_name = localStorage.getItem('user_name');
+  const avatar = localStorage.getItem('avatar');
+
   return (
     <CustomModal isOpen={isOpen} onClose={onClose} className={css.modal}>
-      <div className={css.Container}>
-        <div>
-          <h2 className={css.title}>Welcome to TeamChat!</h2>
-          <p className={css.text}>Check email to finish registration!</p>
+      <div className={css.container}>
+        
+          <div className={css.avatarBorder}>
+            <img src={avatar} alt="User Avatar" className={css.avatar} />
+           </div>
+       
+        
+          <h2 className={css.title}>Hello, <span className={css.accent}>{user_name}</span>!</h2>
+          <h3 className={css.subtitle}>Welcome to the CoolChat</h3>
+          <p className={css.text}>Open the letter in your mail, go through verification and get the opportunity to use the additional features of CoolChat</p>
           <button className={css.button} onClick={onClose}>Close</button>
         </div>
-      </div>
+      
     </CustomModal>
   );
 };
