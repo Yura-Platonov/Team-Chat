f81220553344b0489071ea787d9f128283c86610
Yura <yuraplatonov2000@gmail.com>
2024-03-04 18:07:33 +0200

    add refresh

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import axios from 'axios';
 import * as yup from 'yup';
@@ -7,36 +7,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import css from './LoginForm.module.css';
 
-import AuthContext from './AuthContext';
+import { useAuth } from './AuthContext'; // Изменил импорт на использование useAuth хука
 import LoginErrorModal from '../Modal/LoginErrorModal';
 
 const validationSchema = yup.object().shape({
   username: yup.string()
-  .test('is-valid-email', 'Please input correct email', value => {
-    return (
-      value && /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value)
-    );
-  })
-  .required('Email is required'),
+    .test('is-valid-email', 'Please input correct email', value => {
+      return (
+        value && /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value)
+      );
+    })
+    .required('Email is required'),
 
-  
   password: yup.string()
-  .required('Password is required')
-  .matches(
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*\s)/,
-    'Use at least one: (0-9), (a-z), (A-Z), (@#$%^&+=!)'
-  )
-  .max(8, 'Password must be at most 8 characters long'),
-  });
+    .required('Password is required')
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*\s)/,
+      'Use at least one: (0-9), (a-z), (A-Z), (@#$%^&+=!)'
+    )
+    .max(8, 'Password must be at most 8 characters long'),
+});
 
 const LoginForm = ({ onClose, showRegistrationForm }) => {
-  const { login } = useContext(AuthContext);
+  const { login } = useAuth();
   const [showPassword, setShowPassword] = useState(false);
   const [showLoginErrorModal, setShowLoginErrorModal] = useState(false);
 
-
   const togglePasswordVisibility = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
+    setShowPassword(prevShowPassword => !prevShowPassword);
   };
 
   return (
@@ -54,7 +52,7 @@ const LoginForm = ({ onClose, showRegistrationForm }) => {
             method: 'POST',
             url: 'https://cool-chat.club/api/login',
             headers: {
-              'Accept': 'application/json',
+              Accept: 'application/json',
               'Content-Type': 'application/x-www-form-urlencoded',
             },
             data,
@@ -63,72 +61,71 @@ const LoginForm = ({ onClose, showRegistrationForm }) => {
           const response = await axios(options);
 
           if (response.status === 200) {
-            const { access_token } = response.data;
+            const { access_token, refresh_token } = response.data;
             localStorage.setItem('access_token', access_token);
+            localStorage.setItem('refresh_token', refresh_token); // Сохраняем refresh token
 
-            login(access_token, values.username);
+            login(access_token, refresh_token, values.username); // Обновленный вызов функции login с передачей refresh token
             onClose();
             console.log('User logged in');
           } else {
             setShowLoginErrorModal(true);
           }
         } catch (error) {
-          console.log(error.message);
+          console.error('Error during login:', error.message);
           setShowLoginErrorModal(true);
         } finally {
           setSubmitting(false);
         }
       }}
     >
-      {({ errors, touched}) => (
+      {({ errors, touched }) => (
         <Form className={css.loginForm}>
-        <h2 className={css.title}>Log in to Coolchat</h2>
-        <div>
-          <label htmlFor="username" className={css.text}>
-            Login
-          </label>
-          <Field
-            // className={` ${touched.username && errors.username ? css.isInvalid : css.isvalid} ${css.input}`}
-            className={`${touched.username && errors.username ? css.isInvalid : touched.username ? css.isValid : ''} ${css.input}`}
-            type="text"
-            id="username"
-            name="username"
-            autoComplete="email"
-            placeholder="name@gmail.com"
-          />
-          <ErrorMessage name="username" component="div" />
-        </div>
-        <div>
-          <label htmlFor="password" className={css.text}>
-            Password
-            <span
-              onClick={togglePasswordVisibility}
-              className={css.passwordToggleIcon}
-            >
-              <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
-            </span>
-          </label>
-          <Field
-            type={showPassword ? 'text' : 'password'}
-            id="password"
-            name="password"
-            autoComplete="current-password"
-            className={`${touched.password && errors.password ? css.isInvalid : touched.password ? css.isValid : ''} ${css.input}`}
-            placeholder="Enter your password"
-          />
-          <ErrorMessage name="password" component="div" />
-        </div>
-        <div className={css.buttonsContainer}>
-          <button type="submit" className={css.button}>
-            Log in
-          </button>
-          <button type="button" className={css.buttonLink} onClick={showRegistrationForm}>
-            Register
-          </button>
-        </div>
-        {showLoginErrorModal && <LoginErrorModal isOpen={true} onClose={() => setShowLoginErrorModal(false)} />}
-      </Form>
-      
+          <h2 className={css.title}>Log in to Coolchat</h2>
+          <div>
+            <label htmlFor="username" className={css.text}>
+              Login
+            </label>
+            <Field
+              className={`${touched.username && errors.username ? css.isInvalid : touched.username ? css.isValid : ''} ${css.input}`}
+              type="text"
+              id="username"
+              name="username"
+              autoComplete="email"
+              placeholder="name@gmail.com"
+            />
+            <ErrorMessage name="username" component="div" />
+          </div>
+          <div>
+            <label htmlFor="password" className={css.text}>
+              Password
+              <span
+                onClick={togglePasswordVisibility}
+                className={css.passwordToggleIcon}
+              >
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+              </span>
+            </label>
+            <Field
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              name="password"
+              autoComplete="current-password"
+              className={`${touched.password && errors.password ? css.isInvalid : touched.password ? css.isValid : ''} ${css.input}`}
+              placeholder="Enter your password"
+            />
+            <ErrorMessage name="password" component="div" />
+          </div>
+          <div className={css.buttonsContainer}>
+            <button type="submit" className={css.button}>
+              Log in
+            </button>
+            <button type="button" className={css.buttonLink} onClick={showRegistrationForm}>
+              Register
+            </button>
+          </div>
+          {showLoginErrorModal && <LoginErrorModal isOpen={true} onClose={() => setShowLoginErrorModal(false)} />}
+        </Form>
       )}
     </Formik>
   );
