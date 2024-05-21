1012955c82e54914e81718e429ace9ab8541c15b
Yura <yuraplatonov2000@gmail.com>
2024-05-21 22:33:32 +0300

    Update Chat.jsx

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -489,6 +489,94 @@ const Chat = () => {
     setEditedMessage(''); 
   };
   
+  // String transliterateAndSanitize(String fileName) {
+  //   final Map<String, String> transliterationMap = {
+  //     'А': 'A',
+  //     'Б': 'B',
+  //     'В': 'V',
+  //     'Г': 'G',
+  //     'Д': 'D',
+  //     'Е': 'E',
+  //     'Ё': 'E',
+  //     'Ж': 'Zh',
+  //     'З': 'Z',
+  //     'И': 'I',
+  //     'Й': 'Y',
+  //     'К': 'K',
+  //     'Л': 'L',
+  //     'М': 'M',
+  //     'Н': 'N',
+  //     'О': 'O',
+  //     'П': 'P',
+  //     'Р': 'R',
+  //     'С': 'S',
+  //     'Т': 'T',
+  //     'У': 'U',
+  //     'Ф': 'F',
+  //     'Х': 'Kh',
+  //     'Ц': 'Ts',
+  //     'Ч': 'Ch',
+  //     'Ш': 'Sh',
+  //     'Щ': 'Shch',
+  //     'Ы': 'Y',
+  //     'Э': 'E',
+  //     'Ю': 'Yu',
+  //     'Я': 'Ya',
+  //     'Ї': 'Yi',
+  //     'І': 'I',
+  //     'Є': 'Ye',
+  //     'Ґ': 'G',
+  //     'а': 'a',
+  //     'б': 'b',
+  //     'в': 'v',
+  //     'г': 'g',
+  //     'д': 'd',
+  //     'е': 'e',
+  //     'ё': 'e',
+  //     'ж': 'zh',
+  //     'з': 'z',
+  //     'и': 'i',
+  //     'й': 'y',
+  //     'к': 'k',
+  //     'л': 'l',
+  //     'м': 'm',
+  //     'н': 'n',
+  //     'о': 'o',
+  //     'п': 'p',
+  //     'р': 'r',
+  //     'с': 's',
+  //     'т': 't',
+  //     'у': 'u',
+  //     'ф': 'f',
+  //     'х': 'kh',
+  //     'ц': 'ts',
+  //     'ч': 'ch',
+  //     'ш': 'sh',
+  //     'щ': 'shch',
+  //     'ы': 'y',
+  //     'э': 'e',
+  //     'ю': 'yu',
+  //     'я': 'ya',
+  //     'ї': 'yi',
+  //     'і': 'i',
+  //     'є': 'ye',
+  //     'ґ': 'g'
+  //   };
+
+  //   String transliteratedFileName = fileName.split('').map((char) {
+  //     if (transliterationMap.containsKey(char)) {
+  //       return transliterationMap[char];
+  //     } else if (char == ' ') {
+  //       return '_';
+  //     } else if (RegExp(r'[a-zA-Z0-9\-_\.]').hasMatch(char)) {
+  //       return char;
+  //     } else {
+  //       return '';
+  //     }
+  //   }).join('');
+
+  //   return transliteratedFileName;
+  // }
   
   const getFileType = (fileUrl) => {
     const extension = getFileExtension(fileUrl).toLowerCase();
