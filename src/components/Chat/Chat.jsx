78a94311599914ae7d6a88f3922907c6d2d38727
Yura <yuraplatonov2000@gmail.com>
2024-05-22 16:52:47 +0300

    made rename files

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -246,13 +246,41 @@ const Chat = () => {
     setSelectedReplyMessageId(null);
   };
 
+  const transliterateAndSanitize = (fileName) => {
+    const transliterationMap = {
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E', 'Ж': 'Zh', 'З': 'Z',
+        'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R',
+        'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch',
+        'Ы': 'Y', 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya', 'Ї': 'Yi', 'І': 'I', 'Є': 'Ye', 'Ґ': 'G', 'а': 'a',
+        'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
+        'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's',
+        'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
+        'ы': 'y', 'э': 'e', 'ю': 'yu', 'я': 'ya', 'ї': 'yi', 'і': 'i', 'є': 'ye', 'ґ': 'g'
+    };
+
+    let transliteratedFileName = fileName.split('').map(char => {
+        if (transliterationMap[char]) {
+            return transliterationMap[char];
+        } else if (char === ' ') {
+            return '_';
+        } else if (/^[a-zA-Z0-9\-_.]$/.test(char)) {
+            return char;
+        } else {
+            return '';
+        }
+    }).join('');
+
+    return transliteratedFileName;
+};
+
   const uploadImage = async () => {
     try {
       const formData = new FormData();
-      formData.append('file', selectedImage);
+      const sanitizedFileName = transliterateAndSanitize(selectedImage.name);
+      const file = new File([selectedImage], sanitizedFileName, { type: selectedImage.type });
+      formData.append('file', file);
 
-      console.log(selectedImage);
-      console.log(formData);
+      console.log(file);
 
       // const response = await axios.post('https://cool-chat.club/api/upload_google/uploadfile/', formData);
       const response = await axios.post('https://cool-chat.club/api/upload/upload-to-supabase/?bucket_name=image_chat', formData);
@@ -489,95 +517,6 @@ const Chat = () => {
     setEditedMessage(''); 
   };
   
-  // String transliterateAndSanitize(String fileName) {
-  //   final Map<String, String> transliterationMap = {
-  //     'А': 'A',
-  //     'Б': 'B',
-  //     'В': 'V',
-  //     'Г': 'G',
-  //     'Д': 'D',
-  //     'Е': 'E',
-  //     'Ё': 'E',
-  //     'Ж': 'Zh',
-  //     'З': 'Z',
-  //     'И': 'I',
-  //     'Й': 'Y',
-  //     'К': 'K',
-  //     'Л': 'L',
-  //     'М': 'M',
-  //     'Н': 'N',
-  //     'О': 'O',
-  //     'П': 'P',
-  //     'Р': 'R',
-  //     'С': 'S',
-  //     'Т': 'T',
-  //     'У': 'U',
-  //     'Ф': 'F',
-  //     'Х': 'Kh',
-  //     'Ц': 'Ts',
-  //     'Ч': 'Ch',
-  //     'Ш': 'Sh',
-  //     'Щ': 'Shch',
-  //     'Ы': 'Y',
-  //     'Э': 'E',
-  //     'Ю': 'Yu',
-  //     'Я': 'Ya',
-  //     'Ї': 'Yi',
-  //     'І': 'I',
-  //     'Є': 'Ye',
-  //     'Ґ': 'G',
-  //     'а': 'a',
-  //     'б': 'b',
-  //     'в': 'v',
-  //     'г': 'g',
-  //     'д': 'd',
-  //     'е': 'e',
-  //     'ё': 'e',
-  //     'ж': 'zh',
-  //     'з': 'z',
-  //     'и': 'i',
-  //     'й': 'y',
-  //     'к': 'k',
-  //     'л': 'l',
-  //     'м': 'm',
-  //     'н': 'n',
-  //     'о': 'o',
-  //     'п': 'p',
-  //     'р': 'r',
-  //     'с': 's',
-  //     'т': 't',
-  //     'у': 'u',
-  //     'ф': 'f',
-  //     'х': 'kh',
-  //     'ц': 'ts',
-  //     'ч': 'ch',
-  //     'ш': 'sh',
-  //     'щ': 'shch',
-  //     'ы': 'y',
-  //     'э': 'e',
-  //     'ю': 'yu',
-  //     'я': 'ya',
-  //     'ї': 'yi',
-  //     'і': 'i',
-  //     'є': 'ye',
-  //     'ґ': 'g'
-  //   };
-
-  //   String transliteratedFileName = fileName.split('').map((char) {
-  //     if (transliterationMap.containsKey(char)) {
-  //       return transliterationMap[char];
-  //     } else if (char == ' ') {
-  //       return '_';
-  //     } else if (RegExp(r'[a-zA-Z0-9\-_\.]').hasMatch(char)) {
-  //       return char;
-  //     } else {
-  //       return '';
-  //     }
-  //   }).join('');
-
-  //   return transliteratedFileName;
-  // }
-  
   const getFileType = (fileUrl) => {
     const extension = getFileExtension(fileUrl).toLowerCase();
 
@@ -615,6 +554,7 @@ const Chat = () => {
   
   const renderFile = (fileUrl) => {
     const fileType = getFileType(fileUrl);
+    console.log(fileType)
 
   
     switch (fileType) {
@@ -646,7 +586,8 @@ const Chat = () => {
           </a>
         );
       default:
-        return <p>Unsupported File Type</p>;
+        // return <p>Unsupported File Type</p>;
+        return <p>{fileUrl}</p>;
     }
   };
   
