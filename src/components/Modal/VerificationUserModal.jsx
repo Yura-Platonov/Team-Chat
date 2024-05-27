62f4c478c61a7f95cf8533755cb5159bddac2a2a
Yura <yuraplatonov2000@gmail.com>
2024-05-27 18:09:11 +0300

    add svg to verifyModal

--- src/components/Modal/VerificationUserModal.jsx
+++ src/components/Modal/VerificationUserModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CustomModal from './CustomModal';
 import css from './VerificationUserModal.module.css';
+import { ReactComponent as VerifyImg} from '../Images/VerifyModal.svg'; 
 
 
 const VerificationUserModal = ({ isOpen, onClose }) => {
@@ -11,6 +12,7 @@ const VerificationUserModal = ({ isOpen, onClose }) => {
     <CustomModal isOpen={isOpen} onClose={onClose} className={css.modal}>
       <div className={css.container}>
         
+      <VerifyImg className={css.verifyImg}/>
           <h2 className={css.title}>To create your room, please verify your email</h2>
         
           <p className={css.text}>The instruction was send to {email} immediatly after registration in the app</p>
