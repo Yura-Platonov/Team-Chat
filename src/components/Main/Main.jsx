d70760ab36ec339f983384a068e3e1c10b71af7f
Yura <yuraplatonov2000@gmail.com>
2024-05-29 18:19:36 +0300

    hide code

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { Outlet } from 'react-router';
-import { Tabs, Tab } from 'components/Tabs/Tabs';
+// import { Tabs, Tab } from 'components/Tabs/Tabs';
 
 import RoomList from '../RoomList/RoomList';
 import css from './Main.module.css';
@@ -12,12 +12,12 @@ function Main() {
         <h1 className={css.welcome_title}>Welcome every tourist <br/> to Coolchat</h1>
         <p className={css.welcome_text}>Chat about a wide variety of tourist equipment.<br/>Communicate, get good advice and choose!</p>
       </section>
-      <Tabs>
+      {/* <Tabs>
         <Tab title="Tab 1">Вася
         </Tab>
         <Tab title="Tab 2">Юра
         </Tab>
-      </Tabs>
+      </Tabs> */}
       <RoomList />
       
     </div>
