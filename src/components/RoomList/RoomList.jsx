861c669ed0a9ac07ffa024353349155423adaf5d
Yura <yuraplatonov2000@gmail.com>
2024-06-27 23:11:31 +0300

    fix link

--- src/components/RoomList/RoomList.jsx
+++ src/components/RoomList/RoomList.jsx
@@ -22,11 +22,11 @@ import IconPeopleOnline from '../Images/IconPeopleOnline.svg';
 //   }, []); 
 
 function RoomList({ rooms, onRoomCreated, selectedRooms, setSelectedRooms, isMoveTabOpen }) {
+  
   const addRoom = (newRoom) => {
     onRoomCreated(newRoom);
   };
 
- 
   const handleRoomSelection = (roomId) => {
     setSelectedRooms((prevSelectedRooms) => {
       if (prevSelectedRooms.includes(roomId)) {
@@ -36,14 +36,24 @@ function RoomList({ rooms, onRoomCreated, selectedRooms, setSelectedRooms, isMov
       }
     });
     console.log('Clicked room ID:', roomId);
+  };
 
+  const handleItemClick = (roomId, e) => {
+    if (isMoveTabOpen) {
+      e.preventDefault();
+      handleRoomSelection(roomId);
+    }
   };
 
   return (
     <div className={css.room_section}>
       <ul className={css.room_list}>
         {rooms.map((room) => (
-          <li className={`${css.room_item} ${selectedRooms.includes(room.id) ? css.room_item_active : ''}`} key={room.id}>
+          <li 
+          className={`${css.room_item} ${selectedRooms.includes(room.id) ? css.room_item_active : ''}`} 
+          key={room.id} 
+          onClick={(e) => handleItemClick(room.id, e)}
+          >
             {isMoveTabOpen && ( 
               <input
                 type="checkbox"
@@ -52,27 +62,51 @@ function RoomList({ rooms, onRoomCreated, selectedRooms, setSelectedRooms, isMov
                 className={css.checkbox}
               />
             )}
-            <Link to={`/chat/${room.id}`}>
-              <div className={css.room_container}>
-                <img className={css.room_img} src={room.image_room} alt={room.name_room} />
-                <p className={css.room_name}>{room.name_room}</p>
-              </div>
-              <div className={`${css.room_description} ${selectedRooms.includes(room.id) ? css.selected : ''}`}>
-                <ul className={css.countList}>
-                  <li className={css.people_count}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 20" className={css.unreadMsgSvg} fill={"#F5FBFF"}>
-                      <rect width="28" height="20" rx="4" fill="current"/>
-                      <path d="M4.00391 3.88227L11.5507 9.74214C12.9942 10.8629 15.0137 10.8629 16.4571 9.74214L24.0039 3.88227" stroke="#024A7A" strokeWidth="1.5" strokeLinecap="round"/>
-                    </svg>
-                    <p className={css.whiteText}>{room.count_messages}</p>
-                  </li>
-                  <li className={css.people_count}>
-                    <img src={IconPeopleOnline} className={css.iconPeopleOnlineSvg} alt="IconPeopleOnline" />
-                    <p className={css.whiteText}>{room.count_users}</p>
-                  </li>
-                </ul>
-              </div>
-            </Link>
+            {!isMoveTabOpen ? (
+              <Link to={`/chat/${room.id}`}>
+                <div className={css.room_container}>
+                  <img className={css.room_img} src={room.image_room} alt={room.name_room} />
+                  <p className={css.room_name}>{room.name_room}</p>
+                </div>
+                <div className={`${css.room_description} ${selectedRooms.includes(room.id) ? css.selected : ''}`}>
+                  <ul className={css.countList}>
+                    <li className={css.people_count}>
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 20" className={css.unreadMsgSvg} fill={"#F5FBFF"}>
+                        <rect width="28" height="20" rx="4" fill="current"/>
+                        <path d="M4.00391 3.88227L11.5507 9.74214C12.9942 10.8629 15.0137 10.8629 16.4571 9.74214L24.0039 3.88227" stroke="#024A7A" strokeWidth="1.5" strokeLinecap="round"/>
+                      </svg>
+                      <p className={css.whiteText}>{room.count_messages}</p>
+                    </li>
+                    <li className={css.people_count}>
+                      <img src={IconPeopleOnline} className={css.iconPeopleOnlineSvg} alt="IconPeopleOnline" />
+                      <p className={css.whiteText}>{room.count_users}</p>
+                    </li>
+                  </ul>
+                </div>
+              </Link>
+            ) : (
+              <>
+                <div className={css.room_container}>
+                  <img className={css.room_img} src={room.image_room} alt={room.name_room} />
+                  <p className={css.room_name}>{room.name_room}</p>
+                </div>
+                <div className={`${css.room_description} ${selectedRooms.includes(room.id) ? css.selected : ''}`}>
+                  <ul className={css.countList}>
+                    <li className={css.people_count}>
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 20" className={css.unreadMsgSvg} fill={"#F5FBFF"}>
+                        <rect width="28" height="20" rx="4" fill="current"/>
+                        <path d="M4.00391 3.88227L11.5507 9.74214C12.9942 10.8629 15.0137 10.8629 16.4571 9.74214L24.0039 3.88227" stroke="#024A7A" strokeWidth="1.5" strokeLinecap="round"/>
+                      </svg>
+                      <p className={css.whiteText}>{room.count_messages}</p>
+                    </li>
+                    <li className={css.people_count}>
+                      <img src={IconPeopleOnline} className={css.iconPeopleOnlineSvg} alt="IconPeopleOnline" />
+                      <p className={css.whiteText}>{room.count_users}</p>
+                    </li>
+                  </ul>
+                </div>
+              </>
+            )}
           </li>
         ))}
         <li className={css.room_item}>
