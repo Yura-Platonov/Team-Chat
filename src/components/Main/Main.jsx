68087d79820f32e692130b60789897bd20d348ce
Yura <yuraplatonov2000@gmail.com>
2024-07-10 18:04:12 +0300

    add button

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import Tabs  from 'components/Tabs/Tabs';
-// import RoomList from '../RoomList/RoomList';
 import css from './Main.module.css';
 
 function Main() {
 
+  const [showPlusItems, setShowPlusItems] = useState(false);
+
+  const togglePlusItems = () => {
+    setShowPlusItems(!showPlusItems);
+  };
+
+
   return (
-    <div>
+    <div className={css.container}>
       <section className={css.welcome}>
         <h1 className={css.welcome_title}>Welcome every tourist <br /> to Coolchat</h1>
         <p className={css.welcome_text}>Chat about a wide variety of tourist equipment.<br />Communicate, get good advice and choose!</p>
       </section>
-      
-      <Tabs >
-    </Tabs>
-  
-      
-      {/* <RoomList /> */}
+      <Tabs />
+      <div className={css.plusButton} onClick={togglePlusItems}>
+        {showPlusItems && (
+          <>
+            <div className={`${css.plusItem} ${css.itemTop}`} >
+              Room
+            </div>
+            <div className={`${css.plusItem} ${css.itemLeft}`} >
+             Tab
+            </div>
+            <div className={`${css.plusItem} ${css.itemDiagonal}`}>
+              +
+            </div>
+          </>
+        )}
+        +
+      </div>
     </div>
   );
 }
